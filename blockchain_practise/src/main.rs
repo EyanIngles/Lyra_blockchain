a80ccede2ea5ef4615e4d8d2b425a9b19e251b2a
eyan ingles <eyaningles29@gmail.com>
2025-06-01 12:43:30 +1000

    Feat: added Create-wallet command in CLI to generate new wallet pair and cleaned up main.rs file

--- blockchain_practise/src/main.rs
+++ blockchain_practise/src/main.rs
@@ -1,25 +1,25 @@
 mod block; // importing the modules created.
 mod blockchain;
+mod client;
 mod network;
 mod wallet;
-mod client;
 
-use client::Path;
 use crate::wallet::UserWallet;
+use blockchain::Blockchain;
+use client::Path;
+use network::P2PNode;
+use std::env;
 use std::fs;
 use std::path;
-use blockchain::Blockchain;
-use network::P2PNode;
-use tokio::test;
-// use wallet::UserWallet;
 use std::sync::{Arc, Mutex};
-use std::env;
+use tokio::test;
 
 #[tokio::main]
-async fn main() { // need to ping and ensure that the server is running, this will then route the data and maybe have the server send the signal
+async fn main() {
+    // need to ping and ensure that the server is running, this will then route the data and maybe have the server send the signal
     // of the blockchain so that the data can be all sent and stored on that blockchain, this method, we are creating a new blockchain every
     // time we are calling 'cargo run' we need conditionals to stop this from happening.
-    
+
     let args: Vec<String> = env::args().collect();
 
     if args.len() < 2 {
@@ -29,7 +29,6 @@ async fn main() { // need to ping and ensure that the server is running, this wi
     // sort the command out function here.
     let command_arg: &str = &args[1];
     let command = client::sort_client_args_direction(command_arg);
-
 
     let path = "./blockchain.json";
     let blockchain = if !path::Path::new(path).exists() {
@@ -44,65 +43,20 @@ async fn main() { // need to ping and ensure that the server is running, this wi
         let blockchain: Blockchain = serde_json::from_slice(&file).expect("erorr...");
         blockchain
     };
-   
+
     let blockchain_to_write = Arc::new(Mutex::new(blockchain));
     let p2p_node = P2PNode::new(blockchain_to_write.clone());
 
     // create a match to then point it the a function.
     match command {
         Path::NewBlock => create_new_block(blockchain_to_write.clone(), &args[2]),
-        Path::StartServer => start_server(&p2p_node, args[2].to_string()).await,
         Path::GetBlock => get_block(blockchain_to_write.clone(), args[2].to_string()).await,
-        _ => todo!()
+        Path::StartServer => start_server(&p2p_node, args[2].to_string()).await,
+        Path::CreateWallet => create_wallet(args[2].clone()).await,
+        _ => todo!(),
     };
 
     println!("command here::{:?}", command);
-
-
-    let is_requesting_new_wallet = args[1] == "create_wallet";
-    let wallet_name;
-
-    // ✅ Load the blockchain from file instead of creating a new one
-    // mental note, I think that this is just recreating a new blockchain eachtime so a new one is being started.
-
-    // okay, so i need to have a json file that exists and ill have it so that if it isnt found, then create a new file with the file name.
-    // then we will refer to the blockchain details and make sure they match otherwise return err.
-
-    if is_requesting_new_wallet { // this generates a new wallet and attached a name to it. 
-        wallet_name = args[2].clone();
-        let my_wallet = UserWallet::generate_new_wallet(wallet_name.to_string()); // this does work.
-        println!("new wallet:: {:?}", my_wallet);
-        return
-    }    
-    
-    if command == Path::GetWallet { // this is new block..... not getwallet, get wallet is for testing..
-        let address = format!("127.0.0.1:8080");
-        let blockchain_update = blockchain_to_write.lock().unwrap();
-        //println!("blockchain_update variable: {:?}", blockchain_update);
-        //println!("args[2]: {:?}", args[2]);
-        if command == Path::GetBlock {
-            
-            assert!(args[2] != ""); // asserting that args[2] is not an empty string
-            let block_number: usize = args[2].clone().parse().unwrap();
-            let block = blockchain_update.get_block_via_index(block_number); 
-            if block == None {
-                println!("err: Unable to find block info - value has returned as 'none'");
-                return
-            }
-            println!("Block details: {:?}", block);
-            return 
-        }; // else we are expecting it to be data that is wanting to be passed to a new block and added to the chain.
-
-       // let block_data = args[1].clone();
-       // let new_block_data = block_data.replace("_",  " ");
-       // //println!("testing this section to replace _ with an empty space{:?}", new_block_data);
-       // blockchain_update.add_block_to_chain(new_block_data.clone());
-//
-       // println!("✅ New block added! Latest block: {:?}", blockchain_update.get_last_block());
-       let hash = blockchain_update.get_last_block_hash().clone();
-       p2p_node.connect_to_peer(&address, hash).await; // sending the message off to the peer.
-
-    }
 }
 pub fn create_new_block(blockchain: Arc<Mutex<Blockchain>>, data: &str) {
     //let main_server = format!("127.0.0.1:8080"); // main sever..
@@ -115,16 +69,17 @@ pub fn create_new_block(blockchain: Arc<Mutex<Blockchain>>, data: &str) {
     let blockchain_copy = blockchain_lock.clone();
     drop(blockchain_lock);
 
-
     let data_update = serde_json::to_vec(&blockchain_copy).expect("did not searlise");
     let _ = fs::write("./blockchain.json", data_update);
-    return
+    // TODO: create a peer to peer connection with a message of the block. then that message can be
+    // added to each validator? could be more efficient.
+    return;
 }
 async fn new_blockchain() -> Blockchain {
     let blockchain = Blockchain::new();
-    return blockchain
+    return blockchain;
 }
-async fn start_server(p2p_node: &P2PNode, address: String){ 
+async fn start_server(p2p_node: &P2PNode, address: String) {
     if address == "default" || address == "" {
         p2p_node.start_server("127.0.0.1:8080").await;
         //TODO will want to ping to see if socket is clear and then run that socket address if clear.
@@ -134,15 +89,20 @@ async fn start_server(p2p_node: &P2PNode, address: String){
     // setting to 0, will basically need #TODO is to have 0 as a no so the value must changed otherwise revert.
 }
 async fn get_block(blockchain: Arc<Mutex<Blockchain>>, arg1: String) {
-    let index: usize = arg1.parse().expect("Err: Arg is not a number"); 
+    let index: usize = arg1.parse().expect("Err: Arg is not a number");
     let blockchain_lock = blockchain.lock().expect("Could not lock the blockchain");
     let indexed_block = blockchain_lock.get_block_via_index(index);
     println!("blockchain Data: {:?}", indexed_block);
+}
 
-    }
+async fn create_wallet(name: String) {
+    // create check to ensure there is a name and the name is not already in the data base to
+    // ensure not doubles.
+    let _new_wallet = UserWallet::generate_new_wallet(name);
+}
 
 #[test]
-    async fn test_creating_2_blocks() {
+async fn test_creating_2_blocks() {
     let mut blockchain = Blockchain::new(); // creating the blockchain.
     Blockchain::add_block_to_chain(&mut blockchain, "new block created".to_string()); // creating a new block
     assert_eq!(blockchain.block[1].data, "new block created".to_string());
@@ -150,7 +110,11 @@ async fn get_block(blockchain: Arc<Mutex<Blockchain>>, arg1: String) {
     assert_eq!(blockchain.block[2].data, "new block created, 2".to_string()); //checking that the new block has been pushed.
     let block2hash = Blockchain::get_any_block_hash(&blockchain, 1); // block 2 is index 1.
     let block_previous_hash = Blockchain::get_index_block_previous_hash(&blockchain, 2); // this is block 3 but the index is 2.
-    assert_eq!(block2hash, block_previous_hash, "first hash: {:?} second hash: {:?}",block2hash, block_previous_hash );
+    assert_eq!(
+        block2hash, block_previous_hash,
+        "first hash: {:?} second hash: {:?}",
+        block2hash, block_previous_hash
+    );
 }
 #[test]
 async fn test_fn_get_last_block_index_and_hash() {
@@ -158,30 +122,41 @@ async fn test_fn_get_last_block_index_and_hash() {
     Blockchain::add_block_to_chain(&mut blockchain, "new block created".to_string()); // creating a new block
     let (index, hash) = Blockchain::get_last_block_index_and_hash(&mut blockchain);
     let indexed_hash = Blockchain::get_any_block_hash(&mut blockchain, 1);
-    assert_eq!(index, 1 , "index: {:?}", index);
-    assert_eq!(hash, indexed_hash, "expected hash: {:?}, hash got after function: {:?}", hash, indexed_hash);
+    assert_eq!(index, 1, "index: {:?}", index);
+    assert_eq!(
+        hash, indexed_hash,
+        "expected hash: {:?}, hash got after function: {:?}",
+        hash, indexed_hash
+    );
     Blockchain::add_block_to_chain(&mut blockchain, "new block created".to_string()); // creating 2 new blocks
-    Blockchain::add_block_to_chain(&mut blockchain, "new block created".to_string()); 
+    Blockchain::add_block_to_chain(&mut blockchain, "new block created".to_string());
 }
 #[test]
 async fn test_indexed_functions_work() {
     let mut blockchain = Blockchain::new(); // creating the blockchain.
     Blockchain::add_block_to_chain(&mut blockchain, "index1".to_string()); // creating multiple blocks in the blockchain.
-    Blockchain::add_block_to_chain(&mut blockchain, "index2".to_string()); 
-    Blockchain::add_block_to_chain(&mut blockchain, "index3".to_string()); 
-    Blockchain::add_block_to_chain(&mut blockchain, "index4".to_string()); 
-    Blockchain::add_block_to_chain(&mut blockchain, "index5".to_string()); 
+    Blockchain::add_block_to_chain(&mut blockchain, "index2".to_string());
+    Blockchain::add_block_to_chain(&mut blockchain, "index3".to_string());
+    Blockchain::add_block_to_chain(&mut blockchain, "index4".to_string());
+    Blockchain::add_block_to_chain(&mut blockchain, "index5".to_string());
     let (index1, hash1) = Blockchain::get_any_block_index_and_hash(&blockchain, 0);
     let (last_index, _last_hash) = Blockchain::get_last_block_index_and_hash(&blockchain);
     let last_hashh = Blockchain::get_last_block_hash(&blockchain);
-    assert_ne!(index1, 2,"index: {:?} last_index: {:?}", index1, last_index);
-    assert_ne!(hash1, last_hashh, "hash: {:?} last_hash: {:?}", hash1, last_hashh);
+    assert_ne!(
+        index1, 2,
+        "index: {:?} last_index: {:?}",
+        index1, last_index
+    );
+    assert_ne!(
+        hash1, last_hashh,
+        "hash: {:?} last_hash: {:?}",
+        hash1, last_hashh
+    );
 }
 #[test]
-pub async fn testing_connecting_to_peer_not_complete() { 
+pub async fn testing_connecting_to_peer_not_complete() {
     let blockchain = Blockchain::new();
     let _p2p_node = P2PNode::new(Arc::new(Mutex::new(blockchain.clone())));
-    //P2PNode::start_server(&p2p_node, "127.0.0.1:8080").await; // starting default server. 
-    // The above goes on a forever loop so testing will need to kill the server after test. 
-
+    //P2PNode::start_server(&p2p_node, "127.0.0.1:8080").await; // starting default server.
+    // The above goes on a forever loop so testing will need to kill the server after test.
 }
--- blockchain_practise/src/wallet.rs
+++ blockchain_practise/src/wallet.rs
@@ -1,53 +1,50 @@
-
-use k256::{ecdsa::SigningKey, elliptic_curve::rand_core::OsRng};
 use hex;
+use k256::{ecdsa::SigningKey, elliptic_curve::rand_core::OsRng};
 
 #[derive(PartialEq, Debug)]
-pub struct Currency{
+pub struct Currency {
     pub name: String,
-    pub amount: u128
+    pub amount: u128,
 }
 #[derive(PartialEq, Debug)]
-pub struct UserWallet{
+pub struct UserWallet {
     pub name: String,
     pub address: String,
     pub currency_accounts: Currency,
-
 }
 
 impl UserWallet {
-
     pub fn generate_new_wallet(name: String) -> Self {
+        // TODO: add name for native currency to then use
+        // for the currency section.
         let (private_key, public_key) = Self::generate_keys();
         let current_account = Currency {
             name: name.clone(),
             amount: 0,
         };
-        let wallet = UserWallet{
+        let wallet = UserWallet {
             name: name.clone(),
             address: public_key.to_string(),
-            currency_accounts: current_account
+            currency_accounts: current_account,
         };
-        println!("Private Key:{:?}",private_key);
-        println!("Public Key:{:?}",public_key);
+        println!("New Wallet Users: {:?}", wallet.name);
+        println!("Private Key: {:?}", private_key);
+        println!("Public Key: {:?}", public_key);
         wallet
     }
 
     fn generate_keys() -> (String, String) {
-        //let mut rng = rand::thread_rng(); // this a different way of getting a random key.
-        //let mut private_key: [u8; 32] = rng.gen(); // 256-bit random number
-        //private_key = hex::encode(private_key);
+        let private_key = SigningKey::random(&mut OsRng);
+        let public_key = private_key.verifying_key();
 
-    let private_key = SigningKey::random(&mut OsRng);
-    let public_key = private_key.verifying_key();
+        let private_key_hex = hex::encode(private_key.to_bytes());
+        let public_key_hex = hex::encode(public_key.to_encoded_point(false).as_bytes());
 
-    let private_key_hex = hex::encode(private_key.to_bytes());
-    let public_key_hex = hex::encode(public_key.to_encoded_point(false).as_bytes());
-
-    (private_key_hex, public_key_hex)
+        (private_key_hex, public_key_hex)
     }
 
-    //pub fn transfer_currency(&mut self, )
+    //TODO: pub fn transfer_currency(&mut self, ) // create function to transfer token and amount
+    //to another address, if address doesnt exist on chain, bounce back or abort.
 }
 
 #[test]
@@ -57,3 +54,4 @@ fn test_wallet_generating() {
 
     assert_ne!(wallet1, wallet2);
 }
+
